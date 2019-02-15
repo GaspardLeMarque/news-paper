85ceda7b628d01ee461e0c9aa7c2a64d465fbce4
GaspardLeMarque <gleb.ushakov@yahoo.com>
2019-02-15 20:34:27 +0500

    Update articles-container.js

--- articles-container.js
+++ articles-container.js
@@ -1,4 +1,11 @@
 const articles = [
-    {title:'1', text: null},
-    {title:'2', text: null}
+	{title1: 'India withdraws Most Favoured Nation status to Pak', 
+		text1: 'Following the attack in Jammu & Kashmir\'s Pulwama that left 44 CRPF personnel dead, India has withdrawn the MFN 
+		(Most Favoured Nation) status that it had granted to Pakistan. The decision was taken during the Cabinet Committee on Security 
+		meeting held in New Delhi on Friday. India had granted the status to Pakistan in 1996, a year after WTO\'s formation.'}, 
+	{title2: 'UK PM Theresa May loses vote on her Brexit strategy', 
+		text2: 'British Prime Minister Theresa May on Thursday lost the Parliament vote by 303 to 258 on her negotiating strategy to exit the European Union (EU). The pro-Brexit European Research Group of Conservative MPs announced it had taken a \"collective decision\" to abstain because backing the motion would have amounted to supporting efforts to rule out a no-deal Brexit.'},
+	{title3: 'Facebook and US government negotiating multi-billion-dollar fine',
+		text3: 'Facebook is negotiating a multi-billion-dollar fine to settle an investigation of the US Federal Trade Commission (FTC), the Washington Post reported. The probe focused on whether data sharing with Cambridge Analytica and other privacy disputes violated a 2011 agreement with the FTC to safeguard user privacy. Facebook reportedly faces the highest-ever fine imposed by FTC on a technology firm.'}
+	]
 ]
