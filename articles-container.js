78d9edc68825f9e74dcba43650139935f6b19664
GaspardLeMarque <gleb.ushakov@yahoo.com>
2019-02-16 20:48:45 +0500

    Added logic for article text displaying

--- articles-container.js
+++ articles-container.js
@@ -1,20 +1,20 @@
 const articles = [
-	{title: 'India withdraws Most Favoured Nation status to Pak', 
+	{id: 1, title: 'India withdraws Most Favoured Nation status to Pak', 
 		text: `Following the attack in Jammu & Kashmir's Pulwama that left 44 CRPF personnel dead, 
 		India has withdrawn the MFN (Most Favoured Nation) status that it had granted to Pakistan. The decision was taken 
 		during the Cabinet Committee on Security meeting held in New Delhi on Friday. India had granted the status to 
 		Pakistan in 1996, a year after WTO's formation.`}, 
-	{title: 'UK PM Theresa May loses vote on her Brexit strategy', 
+	{id: 2, title: 'UK PM Theresa May loses vote on her Brexit strategy', 
 		text: `British Prime Minister Theresa May on Thursday lost the Parliament vote by 303 to 258 on her negotiating 
 		strategy to exit the European Union (EU). The pro-Brexit European Research Group of Conservative MPs announced 
 		it had taken a "collective decision" to abstain because backing the motion would have amounted to supporting 
 		efforts to rule out a no-deal Brexit.`},
-	{title: 'Facebook and US government negotiating multi-billion-dollar fine',
+	{id: 3, title: 'Facebook and US government negotiating multi-billion-dollar fine',
 		text: `Facebook is negotiating a multi-billion-dollar fine to settle an investigation of the US Federal Trade 
 	 	Commission (FTC), the Washington Post reported. The probe focused on whether data sharing with Cambridge Analytica 
 		and other privacy disputes violated a 2011 agreement with the FTC to safeguard user privacy. Facebook reportedly 
 		faces the highest-ever fine imposed by FTC on a technology firm.`}
 	]
 
-const { id1, id2, id3 } = articles; //create IDs with destructuring assignment
-let clone = Object.assign(IDcontainer, articles); //create container of IDs
+//const { id1, id2, id3 } = articles; //create IDs with destructuring assignment
+//let clone = Object.assign(IDcontainer, articles); //create container of IDs
--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-function displayText() {
+let textContainer = document.getElementById('text-container');
+function displayText(id) {
     console.log('displaying text...')
-}
+    const article = articles.filter(a => a.id == id)[0];
+    textContainer.innerHTML = article.text;
+};
+
 
 let root = document.getElementById('root');
 articles.forEach(article => {
-    root.innerHTML += `<div onclick="displayText()">${article.title}</div>`;
+    root.innerHTML += `<div onclick="displayText(${article.id})">${article.title}</div>`;
 });
+
