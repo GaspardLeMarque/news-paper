849bd7c506318a94f8400d3d24738cb03d38e610
GaspardLeMarque <gaspard.lemark@gmail.com>
2019-02-26 00:35:47 +0500

    Added fetch() to index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,26 @@
-let articles = require('./articles-container').articles;
-console.log(articles)
+//let textContainer = document.getElementById('text-container');
 
-let textContainer = document.getElementById('text-container');
-function displayText(id) {
-    console.log('displaying text...')
-    const article = articles.filter(a => a.id == id)[0];
-    textContainer.innerHTML = article.text;
-};
-
-let root = document.getElementById('root');
-articles.forEach(article => {
-    var p = document.createElement("p");
-    p.onclick = () => displayText(article.id);
-    p.innerHTML = article.title;
-    root.appendChild(p);
+fetch('localhost:3000/articles').then(function(response) {
+  let objectURL = URL.createObjectURL(response);
+  textContainer.src = objectURL;
 });
-//Changed
-let root = document.getElementById('root');
 
-displayTitles(articles)
 
-function displayTitles(artcls) {
-    artcls.forEach(article => {
-        var p = document.createElement("p");
-        p.onclick = () => displayText(article.id);
-        p.innerHTML = article.title;
-        root.appendChild(p);
-    });
-}
+//function displayText(id) {
+//    console.log('displaying text...')
+//    const article = articles.filter(a => a.id == id)[0];
+//    textContainer.innerHTML = article.text;
+//};
+
+//let root = document.getElementById('root');
+
+//displayTitles(articles)
+
+//function displayTitles(artcls) {
+//    artcls.forEach(article => {
+//        var p = document.createElement("p");
+//        p.onclick = () => displayText(article.id);
+//        p.innerHTML = article.title;
+//        root.appendChild(p);
+//    });
+//}
