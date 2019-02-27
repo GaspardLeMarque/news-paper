4935829185a47bc0731f0a1ffc620752dead4c16
GaspardLeMarque <gleb.ushakov@yahoo.com>
2019-02-27 20:33:51 +0500

    Added fetch()

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
         ]
     },
     plugins: [
-        new HtmlWebPackPlugin({title: 'News paper'}),
+        new HtmlWebPackPlugin({title: 'news paper'}),
         new HtmlWebPackRootPlugin({}),
     ]
 };
