7dfe2ef0fd1bfc2ff225cf5953548265711fdcc1
Max Ermolaev <lainiwakurafan@gmail.com>
2019-03-03 18:59:28 +0300

    Display articles from db into console

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express')    
 const app = express()             //create server
 const port = 3000                 //start server
+const mysql = require('mysql');
+
+let con = mysql.createConnection(require('./db.config'));
+
+con.connect(function(err) {
+  if (err) {console.log(err); return;}
+  console.log("Connected!");
+  con.query('select * from articles',(err, result, fields) => {
+    if (err) {console.log(err); return;}
+    console.log(result);
+  })
+});
+
 let articles = require('./articles-container');
 
 app.use(function(req, res, next) {
